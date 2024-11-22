0ffa1037b679a62a517d5bc507c553b2d5d9830c
Francois Nguinbu <francois.nguinbu@epita.fr>
2024-11-22 06:36:01 +0100

    fix: Winston logger could not log errors

--- server/src/config/logger.ts
+++ server/src/config/logger.ts
@@ -1,4 +1,4 @@
-import { createLogger, format, transports } from 'winston';
+import winston, { createLogger, format, transports } from 'winston';
 
 const logger = createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
@@ -8,7 +8,13 @@ const logger = createLogger({
       format: format.combine(
         format.colorize(), // Adds colors
         format.timestamp(),
-        format.printf(({ timestamp, level, message }) => {
+        format.errors({ stack: true }),
+        format.printf(({ timestamp, level, message, stack }) => {
+          if (message && typeof message === 'object' && 'stack' in message && 'message' in message) {
+            // If the message is an Error object, log the message and stack trace
+            return `${timestamp} [${level}]: ${message.message} \nStack: ${message.stack}`;
+          }
+          // If it's not an Error, just log the message
           return `${timestamp} [${level}]: ${message}`;
         })
       )
@@ -25,5 +31,24 @@ const logger = createLogger({
     })
   ]
 });
-
+// Simple way to bypass a kind of bug that prevents from logging error properly
+const originalError = logger.error;
+// Override the `error` method
+logger.error = ((err: unknown) => {
+  if (err && typeof err === 'object' && 'message' in err && 'stack' in err) {
+    // @ts-ignore: We know `err` has a `message` property
+    originalError.call(logger, `${err.message} ${err.stack}`);  // Log the message from the object
+  }
+  else if (err && typeof err === 'object' && 'message' in err) {
+    // @ts-ignore: We know `err` has a `message` property
+    originalError.call(logger, err.message);  // Log the message from the object
+  }
+  else if (err && typeof err === 'string'){
+    // @ts-ignore: We know `err` has a `message` property
+    originalError.call(logger, err);  // Log the message from the object
+  }
+  else{
+    originalError.call(logger, { message: "An unknown error occurred" });
+  }
+}) as winston.LeveledLogMethod;
 export default logger;
